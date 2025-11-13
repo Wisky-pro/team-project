63a9b9f4fc77f8cb140ef3a846e5fee45d6caa36
kevinyang <kaiweny.yang@mail.utoronto.ca>
2025-11-13 13:30:27 -0500

    LogInInteractor

--- src/Main/java/use_case/LogIn/LogInInteractor.java
+++ src/Main/java/use_case/LogIn/LogInInteractor.java
@@ -1,5 +1,12 @@
 package use_case.LogIn;
 
-public interface LogInInteractor {
+public class LogInInteractor implements LogInInputBoundary {
 
+    @Override
+    public void execute(LogInInputData inputData) {
+        String username = inputData.getUsername();
+        String password = inputData.getPassword();
+
+        System.out.println("Attempting login for: " + username);
+    }
 }
