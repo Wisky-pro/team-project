2fe906f4b9f03f3969b15dcdc00271b3da34d245
williamle <william.le@mail.utoronto.ca>
2025-11-23 14:21:34 -0500

    finished CartItemTest

--- src/test/java/CartItemTest.java
+++ src/test/java/CartItemTest.java
@@ -1,2 +1,31 @@
+package entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CartItemTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+        CartItem item = new CartItem("url1", "Item 1", 5.0, 2);
+        assertEquals("url1", item.getProductUrl());
+        assertEquals("Item 1", item.getName());
+        assertEquals(5.0, item.getPrice(), 0.0001);
+        assertEquals(2, item.getQuantity());
+    }
+
+    @Test
+    public void testIncreaseQuantity() {
+        CartItem item = new CartItem("url1", "Item 1", 5.0, 2);
+        item.increaseQuantity(3);
+        assertEquals(5, item.getQuantity());
+    }
+
+    @Test
+    public void testDecreaseQuantity() {
+        CartItem item = new CartItem("url1", "Item 1", 5.0, 5);
+        item.decreaseQuantity(2);
+        assertEquals(3, item.getQuantity());
+    }
 }
